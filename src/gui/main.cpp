09dec28d4694379241512be8c71a60a5d758b0e2
KaroqDave <davidfuhrmann1@gmail.com>
2026-06-07 19:16:51 +0200

    Update CMake configuration and resource files for Icons support
    - Added resource file for Windows application icon.
    - Updated CMakeLists.txt to include the resource file and link bcrypt for hardware-accelerated hashing on Windows.
    - Set application window icon in main.cpp to use the newly added icon resource.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -2,10 +2,13 @@
 #include "gui/theme.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    QApplication::setWindowIcon(QIcon(QStringLiteral(":/icons/app.ico")));
 
     iso::applyTheme(app, iso::Theme::System);
 
